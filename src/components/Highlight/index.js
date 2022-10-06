6a93038e8ccadef53dd88c6af0fdc7b92329037e
tinh2044 <tinhnguyen23122004@gmail.com>
2022-10-06 21:30:33 +0700

    up

--- src/components/Highlight/index.js
+++ src/components/Highlight/index.js
@@ -1,19 +1,15 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
-import HighlightCard from "./HighlightCard";
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import HighlightCard from './HighlightCard';
 
 export default function Highlight({ summary }) {
-  return (
-    <Grid container spacing={3}>
-      {summary.map((data, index) => (
-        <Grid item key={index} sm={4} xs={12}>
-          <HighlightCard
-            title={data.title}
-            count={data.count}
-            type={data.type}
-          />
+    return (
+        <Grid container spacing={3}>
+            {summary.map((data, index) => (
+                <Grid item key={index} sm={4} xs={12}>
+                    <HighlightCard title={data.title} count={data.count} type={data.type} />
+                </Grid>
+            ))}
         </Grid>
-      ))}
-    </Grid>
-  );
+    );
 }
