e87bf13dd216e65c9bdaee7b1083448f5522ecd7
tinh2044 <tinhnguyen23122004@gmail.com>
2022-10-07 11:19:09 +0700

    up

--- src/components/Charts/HighMaps/index.js
+++ src/components/Charts/HighMaps/index.js
@@ -1,95 +1,93 @@
-import React, { useEffect, useRef, useState } from "react";
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
-import highchartsMap from "highcharts/modules/map";
-import { cloneDeep } from "lodash";
+import React, { useEffect, useRef, useState } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import highchartsMap from 'highcharts/modules/map';
+import { cloneDeep } from 'lodash';
 
 // Load Highcharts modules
 highchartsMap(Highcharts);
 
 const initOptions = (colorMap, titleTooltip) => {
-  return {
-    chart: {
-      height: "500",
-    },
-    title: {
-      text: null,
-    },
-    mapNavigation: {
-      enabled: true,
-    },
-    colorAxis: {
-      min: 0,
-      stops: colorMap,
-    },
-    legend: {
-      layout: "vertical",
-      align: "right",
-      verticalAlign: "bottom",
-    },
-    series: [
-      {
-        name: titleTooltip,
-        joinBy: ["hc-key", "key"],
-      },
-    ],
-  };
+    return {
+        chart: {
+            height: '500',
+        },
+        title: {
+            text: null,
+        },
+        mapNavigation: {
+            enabled: true,
+        },
+        colorAxis: {
+            min: 0,
+            stops: colorMap,
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'bottom',
+        },
+        series: [
+            {
+                name: titleTooltip,
+                joinBy: ['hc-key', 'key'],
+            },
+        ],
+    };
 };
 
 const HighMaps = ({ mapData, colorMap, titleTooltip }) => {
-  console.log(colorMap);
-  const [options, setOptions] = useState({});
-  const [mapLoaded, setMapLoaded] = useState(false);
-  const chartRef = useRef(null);
+    const [options, setOptions] = useState({});
+    const [mapLoaded, setMapLoaded] = useState(false);
+    const chartRef = useRef(null);
 
-  useEffect(() => {
-    if (mapData && Object.keys(mapData).length) {
-      console.log({ mapData });
-      const fakeData = mapData.features.map((feature, index) => ({
-        key: feature.properties["hc-key"],
-        value: index,
-      }));
+    useEffect(() => {
+        if (mapData && Object.keys(mapData).length) {
+            const fakeData = mapData.features.map((feature, index) => ({
+                key: feature.properties['hc-key'],
+                value: index,
+            }));
 
-      setOptions(() => ({
-        ...initOptions(colorMap, titleTooltip),
-        title: {
-          text: mapData.title,
-        },
-        series: [
-          {
-            ...initOptions(colorMap, titleTooltip).series[0],
-            mapData: mapData,
-            data: fakeData,
-          },
-        ],
-      }));
+            setOptions(() => ({
+                ...initOptions(colorMap, titleTooltip),
+                title: {
+                    text: mapData.title,
+                },
+                series: [
+                    {
+                        ...initOptions(colorMap, titleTooltip).series[0],
+                        mapData: mapData,
+                        data: fakeData,
+                    },
+                ],
+            }));
 
-      if (!mapLoaded) setMapLoaded(true);
-    }
-  }, [mapData, mapLoaded, colorMap, titleTooltip]);
+            if (!mapLoaded) setMapLoaded(true);
+        }
+    }, [mapData, mapLoaded, colorMap, titleTooltip]);
 
-  useEffect(() => {
-    if (chartRef && chartRef.current) {
-      chartRef.current.chart.series[0].update({
-        mapData,
-      });
-    }
-  }, [options, mapData]);
+    useEffect(() => {
+        if (chartRef && chartRef.current) {
+            chartRef.current.chart.series[0].update({
+                mapData,
+            });
+        }
+    }, [options, mapData]);
 
-  if (!mapLoaded) return null;
+    if (!mapLoaded) return null;
 
-  return (
-    <HighchartsReact
-      highcharts={Highcharts}
-      options={cloneDeep(options)}
-      constructorType={"mapChart"}
-      ref={chartRef}
-    />
-  );
+    return (
+        <HighchartsReact
+            highcharts={Highcharts}
+            options={cloneDeep(options)}
+            constructorType={'mapChart'}
+            ref={chartRef}
+        />
+    );
 };
 
 HighMaps.defaultProps = {
-  mapData: {},
+    mapData: {},
 };
 
 export default HighMaps;
--- src/components/Charts/LineChart/index.js
+++ src/components/Charts/LineChart/index.js
@@ -1,189 +1,200 @@
-import React, { useEffect, useState } from "react";
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
-import moment from "moment";
-import { Button, ButtonGroup } from "@material-ui/core";
-import { red, green, grey } from "@material-ui/core/colors";
+import React, { useEffect, useState } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import moment from 'moment';
+import { Button, ButtonGroup } from '@material-ui/core';
+import { red, green, grey } from '@material-ui/core/colors';
 
 const generateOptions = (data, type, color, name, titleTooltip) => {
-  const categories = data.map((item) => moment(item.Date).format("DD/MM/YYYY"));
+    const categories = data.map((item) => moment(item.Date).format('DD/MM/YYYY'));
 
-  return {
-    chart: {
-      height: 500,
-    },
-    title: {
-      text: `The chart shows the total number of ${name}`,
-    },
-    xAxis: {
-      categories: categories,
-      crosshair: true,
-    },
-    colors: [color],
-    yAxis: {
-      min: 0,
-      title: {
-        text: null,
-      },
-      labels: {
-        align: "right",
-      },
-    },
-    tooltip: {
-      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-      pointFormat:
-        '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        '<td style="padding:0"><b>{point.y} ca</b></td></tr>',
-      footerFormat: "</table>",
-      shared: true,
-      useHTML: true,
-    },
-    plotOptions: {
-      column: {
-        pointPadding: 0.2,
-        borderWidth: 0,
-      },
-    },
-    series: [
-      {
-        name: titleTooltip,
-        data: data,
-      },
-    ],
-  };
+    return {
+        chart: {
+            height: 500,
+        },
+        title: {
+            text: `The chart shows the total number of ${name}`,
+        },
+        xAxis: {
+            categories: categories,
+            crosshair: true,
+        },
+        colors: [color],
+        yAxis: {
+            min: 0,
+            title: {
+                text: null,
+            },
+            labels: {
+                align: 'right',
+            },
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat:
+                '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y} ca</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true,
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0,
+            },
+        },
+        series: [
+            {
+                name: titleTooltip,
+                data: data,
+            },
+        ],
+    };
 };
 
-export default function LineChart({
-  data,
-  type,
-  setType,
-  nameChart,
-  titleTooltip,
-}) {
-  const [options, setOptions] = useState({});
-  const [reportType, setReportType] = useState("all");
+export default function LineChart({ data, type, setType, nameChart, titleTooltip }) {
+    const [options, setOptions] = useState({});
+    const [reportType, setReportType] = useState('all');
 
-  const typeColor =
-    type === "confirmed"
-      ? red[900]
-      : type === "recovered"
-      ? green[500]
-      : grey[900];
+    const typeColor = type === 'confirmed' ? red[900] : type === 'recovered' ? green[500] : grey[900];
 
-  useEffect(() => {
-    let customData = [];
-    switch (reportType) {
-      case "all":
-        customData = data;
-        break;
-      case "30":
-        customData = data.slice(Math.max(data.length - 30, 1));
-        break;
-      case "7":
-        customData = data.slice(Math.max(data.length - 7, 1));
-        break;
+    useEffect(() => {
+        let customData = [];
+        switch (reportType) {
+            case 'all':
+                customData = data;
+                break;
+            case '30':
+                customData = data.slice(Math.max(data.length - 30, 1));
+                break;
+            case '7':
+                customData = data.slice(Math.max(data.length - 7, 1));
+                break;
 
-      default:
-        customData = data;
-        break;
-    }
+            default:
+                customData = data;
+                break;
+        }
 
-    setOptions(
-      generateOptions(customData, type, typeColor, nameChart, titleTooltip)
+        setOptions(generateOptions(customData, type, typeColor, nameChart, titleTooltip));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data, reportType, type, typeColor]);
+
+    return (
+        <>
+            <ButtonGroup
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    marginBottom: 12,
+                }}
+            >
+                <ButtonGroup
+                    size="small"
+                    aria-label="small outlined button group"
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                    }}
+                >
+                    <Button
+                        onClick={() => setType('confirmed')}
+                        style={
+                            type === 'confirmed'
+                                ? {
+                                      color: typeColor,
+                                      border: `1.5px solid ${typeColor}`,
+                                      fontWeight: 600,
+                                  }
+                                : {}
+                        }
+                    >
+                        Tnfections
+                    </Button>
+                    <Button
+                        onClick={() => setType('recovered')}
+                        style={
+                            type === 'recovered'
+                                ? {
+                                      color: typeColor,
+                                      border: `1.5px solid ${typeColor}`,
+                                      fontWeight: 600,
+                                  }
+                                : {}
+                        }
+                    >
+                        Recovered
+                    </Button>
+                    <Button
+                        onClick={() => setType('deaths')}
+                        style={
+                            type === 'deaths'
+                                ? {
+                                      color: typeColor,
+                                      border: `1.5px solid ${typeColor}`,
+                                      fontWeight: 600,
+                                  }
+                                : {}
+                        }
+                    >
+                        Deaths
+                    </Button>
+                </ButtonGroup>
+                <ButtonGroup
+                    size="small"
+                    aria-label="small outlined button group"
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                    }}
+                >
+                    <Button
+                        style={
+                            reportType === 'all'
+                                ? {
+                                      color: typeColor,
+                                      border: `1.5px solid ${typeColor}`,
+                                      fontWeight: 600,
+                                  }
+                                : {}
+                        }
+                        onClick={() => setReportType('all')}
+                    >
+                        ALL
+                    </Button>
+                    <Button
+                        style={
+                            reportType === '30'
+                                ? {
+                                      color: typeColor,
+                                      border: `1.5px solid ${typeColor}`,
+                                      fontWeight: 600,
+                                  }
+                                : {}
+                        }
+                        onClick={() => setReportType('30')}
+                    >
+                        30 day before
+                    </Button>
+                    <Button
+                        style={
+                            reportType === '7'
+                                ? {
+                                      color: typeColor,
+                                      border: `1.5px solid ${typeColor}`,
+                                      fontWeight: 600,
+                                  }
+                                : {}
+                        }
+                        onClick={() => setReportType('7')}
+                    >
+                        7 day before
+                    </Button>
+                </ButtonGroup>
+            </ButtonGroup>
+
+            <HighchartsReact highcharts={Highcharts} options={options} />
+        </>
     );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data, reportType, type, typeColor]);
-
-  return (
-    <>
-      <ButtonGroup
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginBottom: 12,
-        }}
-      >
-        <ButtonGroup
-          size="small"
-          aria-label="small outlined button group"
-          style={{
-            display: "flex",
-            justifyContent: "flex-end",
-          }}
-        >
-          <Button
-            onClick={() => setType("confirmed")}
-            style={
-              type === "confirmed"
-                ? {
-                    color: typeColor,
-                    border: `1.5px solid ${typeColor}`,
-                    fontWeight: 600,
-                  }
-                : {}
-            }
-          >
-            Tnfections
-          </Button>
-          <Button
-            onClick={() => setType("recovered")}
-            style={
-              type === "recovered"
-                ? {
-                    color: typeColor,
-                    border: `1.5px solid ${typeColor}`,
-                    fontWeight: 600,
-                  }
-                : {}
-            }
-          >
-            Recovered
-          </Button>
-          <Button
-            onClick={() => setType("deaths")}
-            style={
-              type === "deaths"
-                ? {
-                    color: typeColor,
-                    border: `1.5px solid ${typeColor}`,
-                    fontWeight: 600,
-                  }
-                : {}
-            }
-          >
-            Deaths
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup
-          size="small"
-          aria-label="small outlined button group"
-          style={{
-            display: "flex",
-            justifyContent: "flex-end",
-          }}
-        >
-          <Button
-            color={reportType === "all" ? "secondary" : ""}
-            onClick={() => setReportType("all")}
-          >
-            ALL
-          </Button>
-          <Button
-            color={reportType === "30" ? "secondary" : ""}
-            onClick={() => setReportType("30")}
-          >
-            30 day before
-          </Button>
-          <Button
-            color={reportType === "7" ? "secondary" : ""}
-            onClick={() => setReportType("7")}
-          >
-            7 day before
-          </Button>
-        </ButtonGroup>
-      </ButtonGroup>
-
-      <HighchartsReact highcharts={Highcharts} options={options} />
-    </>
-  );
 }
